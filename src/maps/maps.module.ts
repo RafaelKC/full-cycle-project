c7e7c5691cba29822d897f43dd792f66ddb59226
RafaelKC <rafaelkaua97@gmail.com>
2024-12-11 23:16:13 -0300

    Adding prisma, route, routeDrive and directions

--- src/maps/maps.module.ts
+++ src/maps/maps.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { PlacesController } from './places/places.controller';
+import { PlacesService } from './places/places.service';
+import { Client as GoogleMapsClient } from '@googlemaps/google-maps-services-js';
+import { DirectionsController } from './diractions/directions.controller';
+import { DirectionsService } from './diractions/directions.service';
 
-@Module({})
+@Module({
+  controllers: [PlacesController, DirectionsController],
+  providers: [
+    PlacesService,
+    {
+      provide: GoogleMapsClient,
+      useValue: new GoogleMapsClient(),
+    },
+    DirectionsService,
+  ],
+  exports: [DirectionsService],
+})
 export class MapsModule {}
