cf722ebeaf28599db7fe99e0c147aebeb1fa759b
RafaelKC <rafaelkaua97@gmail.com>
2024-12-11 23:39:02 -0300

    Continue

--- src/routes/dto/create-route.dto.ts
+++
@@ -1,5 +0,0 @@
-export class CreateRouteDto {
-  public name: string;
-  public source_id: string;
-  public destination_id: string;
-}
--- src/routes/dto/route-driver.ts
+++
@@ -1,5 +0,0 @@
-export class RouteDriver {
-  public route_id: string;
-  public lat: number;
-  public lng: number;
-}
